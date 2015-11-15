30421474fea833a782100bcab9ead08ddb1c6871
Nathanael <nmonagha@purdue.edu>
2015-11-15 10:08:08 -0500

    some changes

--- app/src/main/java/edu/nmonaghapurdue/puzzle/MainActivity.java
+++ app/src/main/java/edu/nmonaghapurdue/puzzle/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int difficultly = 0;
+    private int picture = 0;
+    private int location = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,217 +41,257 @@ public class MainActivity extends AppCompatActivity {
 
     public void mediumDifficult(View view) {
         setContentView(R.layout.gameplay);
+        difficultly = 2;
 
     }
 
     public void hardDifficult(View view) {
         setContentView(R.layout.gameplay);
-
+        difficultly = 3;
     }
 
     public void c1(View view) {
         Button c1 = (Button) findViewById(R.id.c1);
         checkColorToChange();
         c1.setBackgroundColor((Color.BLUE));
+        location = 1;
     }
 
     public void c2(View view) {
         Button c2 = (Button) findViewById(R.id.c2);
         checkColorToChange();
         c2.setBackgroundColor(Color.BLUE);
+        location = 2;
     }
 
     public void c3(View view) {
         Button c3 = (Button) findViewById(R.id.c3);
         checkColorToChange();
         c3.setBackgroundColor((Color.BLUE));
+        location = 3;
     }
 
     public void c4(View view) {
         Button c4 = (Button) findViewById(R.id.c4);
         checkColorToChange();
         c4.setBackgroundColor((Color.BLUE));
+        location = 4;
     }
 
     public void c5(View view) {
         Button c5 = (Button) findViewById(R.id.c5);
         checkColorToChange();
         c5.setBackgroundColor((Color.BLUE));
+        location = 5;
     }
 
     public void c6(View view) {
         Button c6 = (Button) findViewById(R.id.c6);
         checkColorToChange();
         c6.setBackgroundColor((Color.BLUE));
+        location = 6;
     }
 
     public void c7(View view) {
         Button c7 = (Button) findViewById(R.id.c7);
         checkColorToChange();
         c7.setBackgroundColor((Color.BLUE));
+        location = 7;
     }
 
     public void c8(View view) {
         Button c8 = (Button) findViewById(R.id.c8);
         checkColorToChange();
         c8.setBackgroundColor((Color.BLUE));
+        location = 8;
     }
 
     public void c9(View view) {
         Button c9 = (Button) findViewById(R.id.c9);
         checkColorToChange();
         c9.setBackgroundColor((Color.BLUE));
+        location = 9;
     }
 
     public void c10(View view) {
         Button c10 = (Button) findViewById(R.id.c10);
         checkColorToChange();
         c10.setBackgroundColor((Color.BLUE));
+        location = 10;
     }
 
     public void c11(View view) {
         Button c11 = (Button) findViewById(R.id.c11);
         checkColorToChange();
         c11.setBackgroundColor((Color.BLUE));
+        location = 11;
     }
 
     public void c12(View view) {
         Button c12 = (Button) findViewById(R.id.c12);
         checkColorToChange();
         c12.setBackgroundColor((Color.BLUE));
+        location = 12;
     }
 
     public void c13(View view) {
         Button c13 = (Button) findViewById(R.id.c13);
         checkColorToChange();
         c13.setBackgroundColor((Color.BLUE));
+        location = 13;
     }
 
     public void c14(View view) {
         Button c14 = (Button) findViewById(R.id.c14);
         checkColorToChange();
         c14.setBackgroundColor((Color.BLUE));
+        location = 14;
     }
 
     public void c15(View view) {
         Button c15 = (Button) findViewById(R.id.c15);
         checkColorToChange();
         c15.setBackgroundColor((Color.BLUE));
+        location = 15;
     }
 
     public void c16(View view) {
         Button c16 = (Button) findViewById(R.id.c16);
         checkColorToChange();
         c16.setBackgroundColor((Color.BLUE));
+        location = 16;
     }
 
     public void c17(View view) {
         Button c17 = (Button) findViewById(R.id.c17);
         checkColorToChange();
         c17.setBackgroundColor((Color.BLUE));
+        location = 17;
     }
 
     public void c18(View view) {
         Button c18 = (Button) findViewById(R.id.c18);
         checkColorToChange();
         c18.setBackgroundColor((Color.BLUE));
+        location = 18;
     }
 
     public void c19(View view) {
         Button c19 = (Button) findViewById(R.id.c19);
         checkColorToChange();
         c19.setBackgroundColor((Color.BLUE));
+        location = 19;
     }
 
     public void c20(View view) {
         Button c20 = (Button) findViewById(R.id.c20);
         checkColorToChange();
         c20.setBackgroundColor((Color.BLUE));
+        location = 20;
     }
 
     public void c21(View view) {
         Button c21 = (Button) findViewById(R.id.c21);
         checkColorToChange();
         c21.setBackgroundColor((Color.BLUE));
+        location = 21;
     }
 
     public void c22(View view) {
         Button c22 = (Button) findViewById(R.id.c22);
         checkColorToChange();
         c22.setBackgroundColor((Color.BLUE));
+        location = 22;
     }
 
     public void c23(View view) {
         Button c23 = (Button) findViewById(R.id.c23);
         checkColorToChange();
         c23.setBackgroundColor((Color.BLUE));
+        location = 23;
     }
 
     public void c24(View view) {
         Button c24 = (Button) findViewById(R.id.c24);
         checkColorToChange();
         c24.setBackgroundColor((Color.BLUE));
+        location = 24;
     }
 
     public void c25(View view) {
         Button c25 = (Button) findViewById(R.id.c25);
         checkColorToChange();
         c25.setBackgroundColor((Color.BLUE));
+        location = 25;
     }
     public void button1(View view) {
         Button b1 = (Button) findViewById(R.id.button1);
         checkShapeColorToChange();
-        b1.setBackgroundColor((Color.BLUE));
+        b1.setTextColor((Color.BLUE));
+        picture = 1;
     }
     public void button2(View view) {
         Button b2 = (Button) findViewById(R.id.button2);
         checkShapeColorToChange();
-        b2.setBackgroundColor((Color.BLUE));
+        b2.setTextColor((Color.BLUE));
+        picture = 2;
     }
     public void button3(View view) {
         Button b3 = (Button) findViewById(R.id.button3);
         checkShapeColorToChange();
-        b3.setBackgroundColor((Color.BLUE));
+        b3.setTextColor((Color.BLUE));
+        picture = 3;
     }
     public void button4(View view) {
         Button b4 = (Button) findViewById(R.id.button4);
         checkShapeColorToChange();
-        b4.setBackgroundColor((Color.BLUE));
+        b4.setTextColor((Color.BLUE));
+        picture = 4;
     }
     public void button5(View view) {
         Button b5 = (Button) findViewById(R.id.button5);
         checkShapeColorToChange();
-        b5.setBackgroundColor((Color.BLUE));
+        b5.setTextColor((Color.BLUE));
+        picture = 5;
     }
     public void button6(View view) {
         Button b6 = (Button) findViewById(R.id.button6);
         checkShapeColorToChange();
-        b6.setBackgroundColor((Color.BLUE));
+        b6.setTextColor((Color.BLUE));
+        picture = 6;
     }
     public void button7(View view) {
         Button b7 = (Button) findViewById(R.id.button7);
         checkShapeColorToChange();
-        b7.setBackgroundColor((Color.BLUE));
+        b7.setTextColor((Color.BLUE));
+        picture = 7;
     }
     public void button8(View view) {
         Button b8 = (Button) findViewById(R.id.button8);
         checkShapeColorToChange();
-        b8.setBackgroundColor((Color.BLUE));
+        b8.setTextColor((Color.BLUE));
+        picture = 8;
     }
     public void button9(View view) {
         Button b9 = (Button) findViewById(R.id.button9);
         checkShapeColorToChange();
-        b9.setBackgroundColor((Color.BLUE));
+        b9.setTextColor((Color.BLUE));
+        picture = 9;
     }
     public void button10(View view) {
         Button b10 = (Button) findViewById(R.id.button10);
         checkShapeColorToChange();
-        b10.setBackgroundColor((Color.BLUE));
+        b10.setTextColor((Color.BLUE));
+        picture = 10;
     }
     public void button11(View view) {
         Button b11 = (Button) findViewById(R.id.button11);
         checkShapeColorToChange();
-        b11.setBackgroundColor((Color.BLUE));
+        b11.setTextColor((Color.BLUE));
+        picture = 11;
+    }
+    public void submit(View view) {
+        ButtonClass b = new ButtonClass(location ,difficultly ,picture);
     }
 
     public void exit(View view) {
@@ -290,8 +332,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void checkShapeColorToChange() {
-        Button defaultColor = (Button) findViewById(R.id.exit);
-        Drawable r = defaultColor.getBackground();
         Button b1 = (Button) findViewById(R.id.button1);
         Button b2 = (Button) findViewById(R.id.button2);
         Button b3 = (Button) findViewById(R.id.button3);
@@ -304,8 +344,8 @@ public class MainActivity extends AppCompatActivity {
         Button b10 = (Button) findViewById(R.id.button10);
         Button b11 = (Button) findViewById(R.id.button11);
         Button[] arr = {b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11};
-        for (int i = 0; i < 10; i++) {
-            arr[i].setBackgroundDrawable(r);
+        for (int i = 0; i < 11; i++) {
+            arr[i].setTextColor(Color.WHITE);
         }
     }
 }
