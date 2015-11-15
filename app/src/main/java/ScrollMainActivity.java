428c99b7da3d4469394b49ab4de428c32c831fed
Peyton Puckett <peytonpuckett@Peytons-MacBook-Pro.local>
2015-11-15 00:00:23 -0500

    whatever

---
+++ app/src/main/java/ScrollMainActivity.java
@@ -0,0 +1,16 @@
+/**
+ * Created by peytonpuckett on 11/14/15.
+ */
+//package com.example.horizontalscrollview;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import edu.nmonaghapurdue.puzzle.R;
+
+public class ScrollMainActivity extends Activity {
+
+    @Override public void onCreate(Bundle savedInstanceState)
+    { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main); }
+
+}
